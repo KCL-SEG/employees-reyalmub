5f506f445502169c2d3305d01b516e083fb5ba7f
Rey Almubarak <raghad.almub@gmail.com>
2023-10-16 21:58:10 +0100

    syntax error in string class and edit the wage calculation

--- employee.py
+++ employee.py
@@ -12,21 +12,22 @@ class Employee:
         self.contractNumber = 0
         self.comPerConstract = 0
         self.bonus = 0
+        self.wage = self.salary
     def get_pay(self):
-        if self.contract is "contract":
-            self.salary = self.hrs * self.salary
-        if self.commissionType is "contract":
-            self.salary += (self.contractNumber * self.comPerConstract)
-        if self.commissionType is "bonus":
-            self.salary += self.bonus
-        return self.salary
+        if self.contract == "contract":
+            self.wage = self.hrs * self.salary
+        if self.commissionType == "contract":
+            self.wage = self.wage + (self.contractNumber * self.comPerConstract)
+        if self.commissionType == "bonus":
+            self.wage += self.bonus
+        return self.wage
 
     def __str__(self):
         description = f'{self.name} works on a {self.contract} of {self.salary} '
-        if self.contract is "contract":
+        if self.contract == "contract":
             description += f'hours at {str(self.hrs)}/hour. '
         if self.commission:
-            if self.commission is "contract":
+            if self.commission == "contract":
                 description += f'receives a commission for {str(self.contractNumber)} contract(s) at {str(self.comPerConstract)}/contract. '
             else: 
                 description += f'receives a {self.commissionType} commission of {str(self.bonus)}.'
@@ -50,4 +51,5 @@ jan = Employee('Jan',"contract",150,25,True,"contract",3,220)
 robbie = Employee('Robbie',"monthly salary",2000,commission=True, commissionType="bonus",bonus=1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-# ariel = Employee('Ariel')
+ariel = Employee('Ariel',"contract",120,30,True,"bonus",bonus=600)
+
