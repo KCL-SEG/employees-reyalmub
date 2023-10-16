5d0da077d2a77a92e9733f027cdfa34a039464cf
Rey Almubarak <raghad.almub@gmail.com>
2023-10-16 21:50:46 +0100

    add the objects in the file

--- employee.py
+++ employee.py
@@ -34,20 +34,20 @@ class Employee:
         return description 
 
 
-# # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-# billie = Employee('Billie',"monthly",4000)
+# Billie works on a monthly salary of 4000.  Their total pay is 4000.
+billie = Employee('Billie',"monthly",4000)
 
-# # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-# charlie = Employee('Charlie')
+# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
+charlie = Employee('Charlie',"contract",100,25)
 
-# # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-# renee = Employee('Renee')
+# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
+renee = Employee('Renee',"monthly salary",3000,commission=True, commissionType="contract",contractNumber= 4, comPerConstract=200)
 
-# # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-# jan = Employee('Jan')
+# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
+jan = Employee('Jan',"contract",150,25,True,"contract",3,220)
 
-# # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-# robbie = Employee('Robbie')
+# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
+robbie = Employee('Robbie',"monthly salary",2000,commission=True, commissionType="bonus",bonus=1500)
 
-# # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
+# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 # ariel = Employee('Ariel')
